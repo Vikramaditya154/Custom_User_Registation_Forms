988531a214c433214bc28ab8297f4ab1538ef252
Vikramaditya154 <arjunroyal6385@gmail.com>
2023-08-17 11:37:34 +0530

    first commit

---
+++ CUSTOM_USER_REGISTATION/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for CUSTOM_USER_REGISTATION project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CUSTOM_USER_REGISTATION.settings')
+
+application = get_asgi_application()
---
+++ app/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+# Create your models here.
+from django.contrib.auth.models import AbstractBaseUser,BaseUserManager,PermissionsMixin
+
+class UserProfileManager(BaseUserManager):
+    def create_user(self,email,f_n,l_n,password=None):
+        if not email:
+            raise ValueError('Email is not required')
+        NE=self.normalize_email(email)
+        UPO=self.model(email=NE,first_name=f_n,last_name=l_n)
+        UPO.set_password(password)
+        UPO.save()
+        return UPO
+    
+    def create_superuser(self,email,first_name,last_name,password):
+        UPO=self.create_user(email,first_name,last_name,password)
+        UPO.is_staff=True
+        UPO.is_superuser=True
+        UPO.save()
+        
+        
+class UserProfile(AbstractBaseUser,PermissionsMixin):
+    email=models.EmailField(max_length=100,primary_key=True)
+    first_name=models.CharField(max_length=100)
+    last_name=models.CharField(max_length=100)
+    is_active=models.BooleanField(default=True)
+    is_staff=models.BooleanField(default=False)
+    is_superuser=models.BooleanField(default=False)
+    objects=UserProfileManager()
+    
+    USERNAME_FIELD='email'
+    REQUIRED_FIELDS=['first_name','last_name']
+    
